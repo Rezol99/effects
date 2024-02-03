ac223eca0ab8f61fdfa0511d4c5d0c2cb7c2c846
Ryota Iizuka <ez.ryow@gmail.com>
2024-02-03 12:53:06 +0900

    add blur image effect

---
+++ effects/blur_image.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+
+
+def blur_image(image: np.ndarray, params: dict) -> np.ndarray:
+    intensity = params.get("intensity", 0)
+    if intensity > 0:
+        out = cv2.blur(image, (intensity, intensity)) # type: ignore
+        return out
+    return image
