473411aba35008776c08805da6420786679f102a
Ryota Iizuka <ez.ryow@gmail.com>
2024-02-05 00:09:52 +0900

    fix param key

--- effects/blur_image.py
+++ effects/blur_image.py
@@ -3,8 +3,8 @@ import cv2
 
 
 def execute(image: np.ndarray, params: dict) -> np.ndarray:
-    intensity = params.get("intensity", 0)
-    if intensity > 0:
-        out = cv2.blur(image, (intensity, intensity)) # type: ignore
+    amount = params.get("amount", 0)
+    if amount > 0:
+        out = cv2.blur(image, (amount, amount)) # type: ignore
         return out
     return image
